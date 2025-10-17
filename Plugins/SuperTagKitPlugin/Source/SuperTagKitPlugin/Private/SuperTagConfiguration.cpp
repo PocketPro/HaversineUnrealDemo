944a4674f9be7a8a0c02135c5ba61f8f8cf7fb5d
Gord Parke <grparke@gmail.com>
2025-10-16 20:39:36 -0500

    Convert SuperTagKitPlugin to git submodule
    - Moved SuperTagKitPlugin to its own repository
    - Added as submodule at git@github.com:SkyhawkeTechnologies/unreal-sdk-haversine.git
    - Preserves HaversineSatelliteLibrary_CPP nested submodule

--- Plugins/SuperTagKitPlugin/Source/SuperTagKitPlugin/Private/SuperTagConfiguration.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SuperTagConfiguration.h"
-
-const FString FSuperTagConfiguration::SkyGolfBaseURL = TEXT("https://qa2-8264ee52f589f4c0191aa94f87aa1aeb-sg360.skygolf.com");
-const FString FSuperTagConfiguration::SkygolfDevKey = TEXT("B0EBD-B20D4-B6AAA-54B63");
